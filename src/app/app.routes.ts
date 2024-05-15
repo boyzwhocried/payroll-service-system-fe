719b8d8380a0b5d4dc7029b849b5548dd4beb234
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-05-15 14:31:29 +0700

    feat(login): Implement login page and routing
    - Update app.routes.ts to include routes for the root path and login page. Routes now use lazy loading for the LoginComponent, improving initial load performance.
    - Implement LoginComponent with reactive form validation and onSubmit logic. This allows for basic login functionality with email and password validation.
    - Add a new HTML template for LoginComponent that provides a user-friendly login interface, complete with form validation and styled components from PrimeNG.
    - Include necessary PrimeNG modules and ReactiveFormsModule to support form controls and UI components in LoginComponent.
    
    This commit sets up the foundational routing and user interface for user authentication, paving the way for backend integration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        title: 'Login',
+        // component: NavbarComponent,
+        loadComponent:() => import('./pages/login/login.component').then(m => m.LoginComponent),
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
