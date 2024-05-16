abc265edc2927ae1a1f29686413538800b99ee2e
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-05-16 11:35:40 +0700

    feat: Add authentication service and improve login component
    - **LoginComponent**:
      - Added a preview of form values using a `<pre>` tag for debugging purposes.
      - Updated the form builder to use `NonNullableFormBuilder` for better type safety.
      - Integrated `AuthService` for handling the login process.
      - Adjusted password field validation to require a minimum length of 3 characters.
      - Modified the `onSubmit` method to use the new `AuthService` for logging in.
    
    - **AuthService**:
      - Created a new service to handle authentication-related tasks.
      - Added methods for logging in, saving login data to local storage, retrieving login data, and fetching the token.
    
    - **DTOs**:
      - Added `LoginReqDto` and `LoginResDto` interfaces for typing the login request and response data.
    
    - **BaseService**:
      - Introduced a base service for making HTTP requests with automatic token handling and error response handling.
      - Configured request headers to include authorization tokens when necessary.
      - Added methods for GET, POST, and PUT requests with optional token inclusion.
    
    - **Module Imports**:
      - Added necessary imports for `HttpClientModule` and other modules in the login component and services.
    
    These changes enhance the login functionality by introducing a structured approach to handling authentication and preparing the codebase for further expansion.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators, ReactiveFormsModule, NonNullableFormBuilder } from '@angular/forms';
 import { ImageModule } from 'primeng/image';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { RippleModule } from 'primeng/ripple';
+import { AuthService } from '../../services/auth/auth.service';
+import { LoginReqDto } from '../../models/dto/login/login.req.dto';
 
 @Component({
   selector: 'app-login',
@@ -18,25 +20,30 @@ import { RippleModule } from 'primeng/ripple';
     RippleModule,
   ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+
 })
 export class LoginComponent {
   loginForm = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    password: ['', [Validators.required, Validators.minLength(3)]],
   });
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private authService: AuthService,
+  ) { }
 
   onSubmit(): void {
     if (this.loginForm.valid) {
       // Execute login logic
       // Example:
-      // this.userService.login(this.loginForm.value).subscribe({
-      //   next: (response) => this.userService.saveLoginData(response),
-      //   error: (error) => console.error('Login failed:', error),
-      //   complete: () => console.log('Login request complete')
-      // });
+      const loginReqDto: LoginReqDto = this.loginForm.getRawValue()
+      this.authService.login(loginReqDto).subscribe({
+        next: (response) => this.authService.saveLoginData(response),
+        error: (error) => console.error('Login failed:', error),
+        complete: () => console.log('Login request complete')
+      });
     } else {
       console.log('Form is not valid:', this.loginForm.errors);
     }
