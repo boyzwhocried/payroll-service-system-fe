f4c2cf3dfb89cef8bb6ea542608c32fa5b3a8d2b
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-05-17 10:03:49 +0700

    refactor: update routing and user components
    - Refactor AppRoutingModule to nest 'users' routes and add 'new user' route.
      - Removed duplicate 'users' path with loadChildren.
      - Nested routes under 'users' for 'new' path and users component.
    
    - Update UsersComponent to include RouterOutlet and ensure form handling.
      - Added RouterOutlet import.
      - Updated form handling and initialization.
    
    - Refactor UserNew component to be standalone and include necessary modules.
      - Imported CommonModule, FormsModule, ReactiveFormsModule, InputTextModule, ButtonModule, DropdownModule, and CalendarModule.
      - Enhanced form validation and initialization.
    
    - General improvements in user table template for better clarity.
      - Removed commented-out code.
      - Ensured consistent usage of p-cellEditor for table cells.

--- src/app/pages/users/new/new.users.component.ts
+++ src/app/pages/users/new/new.users.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from "@angular/core";
 import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'user-new',
-  templateUrl: './new.users.component.html'
+  standalone: true,
+  templateUrl: './new.users.component.html',
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    DropdownModule,
+    CalendarModule,
+    ReactiveFormsModule,
+  ]
 })
-export class UserNew implements OnInit{
+export class UserNew implements OnInit {
   roles: any[] | undefined;
-  roleid: string|undefined;
+  roleid: string | undefined;
 
-  userReqDtoFg = this.fb.group ({
+  userReqDtoFg = this.fb.group({
     username: ['', [Validators.required]],
     phoneNo: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
@@ -22,13 +38,13 @@ export class UserNew implements OnInit{
     companyLogoExtension: [''],
   })
   constructor(
-    private fb : NonNullableFormBuilder
-  ) {}
+    private fb: NonNullableFormBuilder
+  ) { }
 
   showCompanyInput() {
     if (this.roleid == '1') {
       return true
-    }  else {
+    } else {
       return false
     }
   }
@@ -37,6 +53,6 @@ export class UserNew implements OnInit{
     this.roles = [
       { name: 'Payroll Service', value: 0 },
       { name: 'Client', value: 1 }
-  ];
+    ];
   }
 }
