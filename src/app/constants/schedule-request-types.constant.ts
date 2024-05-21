a4bca5ffcb629a1cfc1bc637d3e0ac05a0eb5e80
benDaMint <124543056+benDaMint@users.noreply.github.com>
2024-05-21 15:40:04 +0700

    Update schedule-request-types.constant.ts
    Deleted comments

--- src/app/constants/schedule-request-types.constant.ts
+++ src/app/constants/schedule-request-types.constant.ts
@@ -3,8 +3,4 @@ export enum ScheduleStatusType {
     PENDING_CLIENT_DOCUMENT = 'SQT02',
     PENDING_FEEDBACK = 'SQT03',
     COMPLETED = 'SQT04'
-    // SQT01 = 'Pending Schedule',
-    // SQT02 = 'Pending Client Document',
-    // SQT03 = 'Pending Feedback',
-    // SQT04 = 'Pending Scheduled',
 }
