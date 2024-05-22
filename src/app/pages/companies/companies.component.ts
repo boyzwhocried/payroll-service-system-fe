3d7f1d9e9f7c2115a1d461dc3de2e63fabbcc166
benDaMint <billyyong30@gmail.com>
2024-05-22 11:34:06 +0700

    Updated : - Created isNewLogo boolean variable - Created generatePreviewImage - Added statement to update company data by calling updateCompanyData from companyService - On onRowEditSave, index checking has been moved to complete observable of update company statement above - Created validation on reader.onload to check if new logo has been added or not

--- src/app/pages/companies/companies.component.ts
+++ src/app/pages/companies/companies.component.ts
@@ -12,9 +12,10 @@ import { RippleModule } from 'primeng/ripple';
 import { TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { ToastModule } from 'primeng/toast';
-import { CompanyResDto } from '../../dto/company/company.res.dto';
-import { CompanyService } from '../../services/company/company.service';
 import { firstValueFrom } from 'rxjs';
+import { CompanyResDto } from '../../dto/company/company.res.dto';
+import { UpdateCompanyReqDto } from '../../dto/company/update-company.req.dto';
+import { CompanyService } from '../../services/company/company.service';
 
 @Component({
   selector: 'app-companies',
@@ -43,6 +44,7 @@ export class CompaniesComponent {
 
   isEditing = false;
   isHovered = false;
+  isNewLogo = false;
   originalCompanyData: CompanyResDto | null = null;
 
   companyForm = this.formBuilder.group({
@@ -61,7 +63,7 @@ export class CompaniesComponent {
   constructor(
     private messageService: MessageService,
     private companyService: CompanyService,
-    private formBuilder: NonNullableFormBuilder,
+    private formBuilder: NonNullableFormBuilder
   ) { }
 
   ngOnInit() {
@@ -78,8 +80,11 @@ export class CompaniesComponent {
   }
 
   generateImage(id: string) {
-    
     return this.companyService.getImageUrl(id)
+  }
+  
+  generatePreviewImage(contentData: string | undefined, extension: string | undefined) {
+    return `data:image/${extension};base64,${contentData}`;
   }
 
   onCreateClick() {
@@ -93,22 +98,31 @@ export class CompaniesComponent {
   }
 
   onRowEditSave(company: CompanyResDto) {
-    // const editedCompany: CompanyReqDto = this.companyForm.getRawValue() as CompanyReqDto;
-
-    // // Update the original data with the edited data
-    // const index = this.companies.findIndex(c => c.id === company.id);
-    // if (index !== -1) {
-    //   this.companies[index] = { ...this.companies[index], ...editedCompany };
-    // }
-
-    // // Clear the form group
-    // this.companyForm.reset();
-    // this.isEditing = false;
-    // this.originalCompanyData = null;
-
-    // this.messageService.add({ severity: 'success', summary: 'Success', detail: `User is updated` });
+    if(this.companyForm.valid) {
+      const editedCompany: UpdateCompanyReqDto = this.companyForm.getRawValue()
+      
+      // // Clear the form group
+      this.companyForm.reset();
+      this.isEditing = false;
+      this.originalCompanyData = null;
+      
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: `Company data is updated`
+      });
+      firstValueFrom(this.companyService.updateCompanyData(editedCompany)).then(
+        () => {
+          // // Update the original data with the edited data
+          const index = this.companies.findIndex(c => c.id === company.id)
+          if (index !== -1) {
+            this.companies[index] = { ...this.companies[index], ...editedCompany }
+          }
+        }
+      )
+    }
   }
-
+  
   onRowEditCancel(company: CompanyResDto, index: number) {
     if (this.originalCompanyData) {
       this.companies[index] = { ...this.originalCompanyData };
@@ -117,10 +131,9 @@ export class CompaniesComponent {
     this.companyForm.reset();
     this.isEditing = false;
   }
-
+  
   onSubmit() {
     if(this.companyForm.valid) {
-      
     }
   }
 
@@ -138,8 +151,14 @@ export class CompaniesComponent {
       const reader = new FileReader();
 
       reader.onload = () => {
+        if (this.companyForm.value.companyLogoContent) {
+          this.isNewLogo = true
+        }
         const base64 = (reader.result as string).split(',')[1];
-        this.companyForm.patchValue({ companyLogoContent: base64, companyLogoExtension: file.type.split('/')[1] })
+        this.companyForm.patchValue({
+          companyLogoContent: base64,
+          companyLogoExtension: file.type.split('/')[1]
+        })
       };
 
       reader.readAsDataURL(file);
