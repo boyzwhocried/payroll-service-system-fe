ae9e07638bdf0f25d5167a1e6292bfec45ccc867
benDaMint <billyyong30@gmail.com>
2024-05-24 16:11:22 +0700

    Updated : - Added psUserName on ClientListResDto - Added profilePictureId on ClientResDto and PsListResDto

--- src/app/dto/user/client-list.res.dto.ts
+++ src/app/dto/user/client-list.res.dto.ts
@@ -1,6 +1,7 @@
 import { ClientResDto } from "./client.res.dto";
 
 export interface ClientListResDto {
+    psUserName: string,
     assignedClients: ClientResDto[],
     unassignedClients: ClientResDto[]
 }
--- src/app/dto/user/client.res.dto.ts
+++ src/app/dto/user/client.res.dto.ts
@@ -2,5 +2,6 @@ export interface ClientResDto {
     id: string,
     fullName: string,
     email: string,
-    phoneNumber: string
+    phoneNumber: string,
+    profilePictureId: string
 }
