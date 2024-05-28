6e48ef5e65ba1c3121724a11b65ee4ad3f73bf06
StoicCat <josepgultom25@gmail.com>
2024-05-28 13:37:03 +0700

    Fixed payroll stepper UI

--- src/app/constants/schedule-status.const.ts
+++ src/app/constants/schedule-status.const.ts
@@ -1,6 +1,6 @@
 export enum ScheduleStatusType {
-    SQT01 = 'Pending Schedule',
-    SQT02 = 'Pending Client Document',
-    SQT03 = 'Pending Feedback',
-    SQT04 = 'Pending Scheduled',
+  SQT01 = 'Pending Schedule',
+  SQT02 = 'Pending Client Document',
+  SQT03 = 'Pending Feedback',
+  SQT04 = 'Pending Scheduled',
 }
