0e75056d016d61b91cac0001e9e60b23b371b82a
benDaMint <billyyong30@gmail.com>
2024-05-30 14:13:51 +0700

    Added : - general-response on InsertResDto import location

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from '../base/base.service';
-import { InsertResDto } from '../../dto/insert.res.dto';
-import { UpdateResDto } from '../../dto/update.res.dto';
+import { InsertResDto } from '../../dto/general-response/insert.res.dto';
+import { UpdateResDto } from '../../dto/general-response/update.res.dto';
 import { UserReqDto } from '../../dto/user/user.req.dto';
 import { UserResDto } from '../../dto/user/user.res.dto';
+import { ProfileResDto } from '../../dto/user/profile.res.dto';
+import { PasswordReqDto } from '../../dto/user/password.req.dto';
+import { UpdateUserReqDto } from '../../dto/user/update-user.req.dto';
+import { environment } from '../../../env/environment.prod';
 
 
 @Injectable({
@@ -31,5 +35,21 @@ export class UserService {
 
   editUser(user: UserReqDto) {
     return this.baseService.patch<UpdateResDto>(`users`, user)
+  }
+
+  getProfile() {
+    return this.baseService.get<ProfileResDto>('users/profile')
+  }
+
+  getImageUrl(id?: string) {
+    return `${environment.backEndBaseUrl}:${environment.port}/files/${id}`
+  }
+
+  updateUser(data: UpdateUserReqDto) {
+    return this.baseService.patch<UpdateResDto>('users', data)
+  }
+
+  updatePassword(data: PasswordReqDto) {
+    return this.baseService.patch<UpdateResDto>('users/password', data)
   }
 }
