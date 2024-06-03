904d02df69dd05c7c72495ee1ddb9564cde6f5eb
benDaMint <billyyong30@gmail.com>
2024-06-03 13:50:22 +0700

    Updated : - created count, countObserver, countObservable - created firstValueFrom observable execution statement on getNotificationCount() method - added validation and parameter isActive on deleteNotification method - created file, fileObserver, fileObservable - created firstValueFrom observable execution statement on updateUser() method

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -1,21 +1,31 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnInit } from "@angular/core";
 import { BaseService } from "../base/base.service";
 import { NotificationResDto } from "../../dto/notification/notification.res.dto";
 import { environment } from "../../../env/environment.prod";
 import { UpdateResDto } from "../../dto/general-response/update.res.dto";
 import { DeleteResDto } from "../../dto/general-response/delete.res.dto";
+import { Observable, firstValueFrom } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
   })
 
 export class NotificationService {
+    count: number = 0
+    countObserver: any
+    countObservable = new Observable<number>(subscriber => this.countObserver = subscriber)
+    
     constructor(
         private baseService: BaseService
     ) {}
 
     getNotificationCount() {
-        return this.baseService.get<number>('notifications/count')
+        firstValueFrom(this.baseService.get<number>('notifications/count')).then(
+            response => {
+                this.count = response
+                this.countObserver.next(this.count)
+            }
+        )
     }
 
     getNotifications() {
@@ -26,7 +36,11 @@ export class NotificationService {
         return `${environment.backEndBaseUrl}:${environment.port}/files/${id}`
     }
 
-    deleteNotification(id: string) {
+    deleteNotification(id: string, isActive: boolean) {
+        if(isActive) {
+            this.count -= 1
+            this.countObserver.next(this.count)
+        }
         return this.baseService.delete<DeleteResDto>(`notifications/${id}`)
     }
 
