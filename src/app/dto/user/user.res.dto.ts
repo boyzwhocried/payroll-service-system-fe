88a906ad19f9fba0e8a39acdcfa51e6a35136ed2
StoicCat <josepgultom25@gmail.com>
2024-06-07 10:38:49 +0700

    Added edit user and company feature, also fixed edit profile and navbar data

--- src/app/dto/user/user.res.dto.ts
+++ src/app/dto/user/user.res.dto.ts
@@ -1,8 +1,10 @@
 export interface UserResDto {
-    id: string
-    userName: string
-    email: string
-    roleName: string
-    phoneNumber: string
-    profilePictureId : string
+  id: string;
+  userName: string;
+  email: string;
+  roleName: string;
+  phoneNumber: string;
+  profilePictureId: string;
+  profilePictureContent: string;
+  profilePictureExtension: string;
 }
