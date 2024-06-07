25fe6adc9bf37d547ef7f3c2899db9dff5bf1b46
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-06-07 19:19:48 +0700

    refactor(client-assignment): Improve UI and loading experience
    This commit refactors the `assign-client` component to enhance the user interface and loading experience. Changes include:
    
    - Added a loading skeleton to display placeholders while data is being fetched.
    - Improved button layout and appearance for better usability.
    - Organized imports and added necessary modules (`SkeletonModule`, `ImageModule`).
    - Updated the component initialization to asynchronously handle data retrieval.
    - Optimized client list rendering for improved performance.
    - Ensured consistency in code formatting and style.
    
    These changes aim to enhance the overall user experience and maintainability of the `assign-client` component.

--- src/app/pages/assign/assign-client/assign-client.component.ts
+++ src/app/pages/assign/assign-client/assign-client.component.ts
@@ -21,6 +21,8 @@ import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { ChipModule } from 'primeng/chip';
 import { UserResDto } from "../../../dto/user/user.res.dto";
 import { ClientAssignmentReqDto } from "../../../dto/client-assignment/client-assignment.req.dto";
+import { SkeletonModule } from "primeng/skeleton";
+import { ImageModule } from "primeng/image";
 
 @Component({
     selector: 'assign-client',
@@ -39,24 +41,29 @@ import { ClientAssignmentReqDto } from "../../../dto/client-assignment/client-as
         AvatarModule,
         AvatarGroupModule,
         ScrollPanelModule,
-        ChipModule
+        ChipModule,
+        SkeletonModule,
+        ImageModule,
     ],
     templateUrl: 'assign-client.component.html'
 })
 
 export class AssignClientComponent implements OnInit {
     clientList: ClientListResDto | undefined
-    
+
     psId: string = ''
     psUserName: string = ''
-    
+
     assignedClient: ClientResDto[] = []
     assignedLength: number | undefined
 
     unassignedClient: ClientResDto[] = []
-    
+
     newlyAssigned: ClientResDto[] = []
-    
+
+    isLoading = true
+    unassignedClientSkeleton: string[] = []
+
 
     clientAssignmentReq = this.formBuilder.group({
         psId: ['', [Validators.required]],
@@ -68,27 +75,25 @@ export class AssignClientComponent implements OnInit {
         private activatedRoute: ActivatedRoute,
         private formBuilder: NonNullableFormBuilder,
         private router: Router
-    ) {}
+    ) { }
 
-    ngOnInit(): void {
-        this.init()
-    }
-
-    init() {
-        firstValueFrom(this.activatedRoute.params).then(
+    async ngOnInit(): Promise<void> {
+        this.isLoading = true
+        this.unassignedClientSkeleton = Array.from({ length: 3 }).map((_, i) => `Item #${i}`);
+        await firstValueFrom(this.activatedRoute.params).then(
             (param) => {
                 this.psId = param['id']
                 this.clientAssignmentReq.patchValue({
-                    psId : this.psId
+                    psId: this.psId
                 })
                 firstValueFrom(this.assignService.getClientList(this.psId)).then(
                     res => {
-                        console.log(res)
                         this.clientList = res
                         this.psUserName = this.clientList.psUserName
                         this.assignedClient = this.clientList.assignedClients
                         this.assignedLength = this.assignedClient.length
                         this.unassignedClient = this.clientList.unassignedClients
+                        this.isLoading = false
                     }
                 )
             }
@@ -111,8 +116,8 @@ export class AssignClientComponent implements OnInit {
 
     isClientSelected(id: string) {
         const size = this.clients.length
-        for(let i=0;i<size;i++) {
-            if(this.clients.at(i).value == id) {
+        for (let i = 0; i < size; i++) {
+            if (this.clients.at(i).value == id) {
                 return false
             }
         }
@@ -139,11 +144,10 @@ export class AssignClientComponent implements OnInit {
         const clientId: string = client.id
         const size = this.clients.length
         this.assignedClient.splice(index, 1)
-        
-        for(let i=0;i<size;i++) {
+
+        for (let i = 0; i < size; i++) {
             const currentId: string = this.clients.at(i).value
-            console.log(currentId + " " + clientId)
-            if(currentId == client.id) {
+            if (currentId == client.id) {
                 this.clients.removeAt(i)
                 break
             }
@@ -152,8 +156,7 @@ export class AssignClientComponent implements OnInit {
     }
 
     onSubmit() {
-        console.log('submitted')
-        if(this.clientAssignmentReq.valid) {
+        if (this.clientAssignmentReq.valid) {
             const clientAssignmentReqDto: ClientAssignmentReqDto = {
                 psId: this.clientAssignmentReq.value.psId as string,
                 clients: this.clientAssignmentReq.value.clients as string[]
