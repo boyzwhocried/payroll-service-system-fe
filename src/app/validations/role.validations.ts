5b71a5cae2058652e7ee1c76a4a26648049140db
boyzwhocried <verrel.alsyoumi@gmail.com>
2024-06-08 16:30:41 +0700

    feat: integrate PrimeNG Toast notifications and MessageService
    - Added `<p-toast>` component in `app.component.html` for PrimeNG toast notifications.
    - Imported `ToastModule` and updated `AppComponent` to include it.
    - Added `MessageService` to the application configuration (`appConfig`).
    - Replaced `ToastrService` with `MessageService` in `BaseService` for error and success handling.
    - Updated `BaseService` to use PrimeNG `MessageService` for displaying notifications.
    
    Changes:
    - `app.component.html`: Added `<p-toast>` component.
    - `app.component.ts`: Imported `ToastModule` and added to `imports`.
    - `app.config.ts`: Imported `MessageService` and added to `providers`.
    - `base.service.ts`: Replaced `ToastrService` with `MessageService` for notification handling.

--- src/app/validations/role.validations.ts
+++ src/app/validations/role.validations.ts
@@ -1,20 +1,20 @@
 import { inject } from "@angular/core";
 import { CanMatchFn, Route, Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
 import { AuthService } from "../services/auth/auth.service";
+import { MessageService } from "primeng/api";
 
-export const roleValidation : CanMatchFn = (route : Route) => {
-  const roleData : string[] = route.data as any
+export const roleValidation: CanMatchFn = (route: Route) => {
+  const roleData: string[] = route.data as any
   const authService = inject(AuthService)
   const dataLogin = authService.getLoginData()
   const router = inject(Router)
-  const toastr = inject(ToastrService)
+  const messageService = inject(MessageService)
 
-  if(dataLogin) {
-    if(roleData.includes(dataLogin.roleCode)) {
+  if (dataLogin) {
+    if (roleData.includes(dataLogin.roleCode)) {
       return true
     } else {
-      toastr.warning('Forbidden Page')
+      messageService.add({ severity: 'warn', summary: 'Warn', detail: 'Forbidden Page' })
       return router.navigateByUrl('/forbidden')
     }
   }
