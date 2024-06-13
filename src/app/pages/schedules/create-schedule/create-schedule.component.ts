7e1c95a5e5fbd804da2b961e9648ad643f24ba4a
StoicCat <josepgultom25@gmail.com>
2024-06-14 00:31:56 +0700

    Fixed : Bugs on application, including : can be rescheduled check feature, route link to stepper page, showing payroll date on schedule list, and fixing create schedule feature.

--- src/app/pages/schedules/create-schedule/create-schedule.component.ts
+++ src/app/pages/schedules/create-schedule/create-schedule.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule, Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
@@ -10,7 +16,6 @@ import { DocumentsReqDto } from '../../../dto/document/documents.req.dto';
 import { DocumentReqDto } from '../../../dto/document/document.req.dto';
 import { firstValueFrom } from 'rxjs';
 import { DocumentService } from '../../../services/document.service';
-
 
 @Component({
   selector: 'app-create-schedule',
@@ -22,122 +27,172 @@ import { DocumentService } from '../../../services/document.service';
     FormsModule,
     ReactiveFormsModule,
     InputTextModule,
-    CalendarModule
+    CalendarModule,
   ],
   templateUrl: './create-schedule.component.html',
-  styleUrl: './create-schedule.component.css'
+  styleUrl: './create-schedule.component.css',
 })
-export class CreateScheduleComponent implements OnInit{
-  scheduleId! : string
-  payrollDate : string = ''
-  maxDate : Date | null = null
-  documents: DocumentsReqDto[] = []
-  scheduleForm! : any
+export class CreateScheduleComponent implements OnInit {
+  scheduleId!: string;
+  payrollDate: string = '';
+  maxDate: Date | null = null;
+  minDate: Date | null = null;
+  documents: DocumentsReqDto[] = [];
+  scheduleForm!: any;
 
   activityInit: DocumentsReqDto[] = [
     {
       activity: 'Employee Data',
-      documentDeadline: ''
+      documentDeadline: '',
     },
     {
       activity: 'Attendance Data',
-      documentDeadline: ''
+      documentDeadline: '',
     },
     {
       activity: 'Payroll Data',
-      documentDeadline: ''
+      documentDeadline: '',
     },
-  ]
-
+  ];
 
   constructor(
     private route: ActivatedRoute,
-    private fb : NonNullableFormBuilder,
+    private fb: NonNullableFormBuilder,
     private location: Location,
     private documentService: DocumentService
   ) {}
 
   ngOnInit(): void {
-    this.scheduleId = this.route.snapshot.queryParamMap.get('id') as string
-    this.payrollDate = this.route.snapshot.queryParamMap.get('payrollDate') as string
-    this.maxDate = this.setMaxDate(this.payrollDate)
+    this.scheduleId = this.route.snapshot.queryParamMap.get('id') as string;
+    this.payrollDate = this.route.snapshot.queryParamMap.get(
+      'payrollDate'
+    ) as string;
+    this.minDate = this.setMinDate();
+    this.maxDate = this.setMaxDate(this.payrollDate);
     this.scheduleForm = this.fb.group({
       scheduleId: [this.scheduleId, [Validators.required]],
-      documentsReqDto: this.fb.array(this.documents)
-    })
-    this.activityOnInit()
+      documentsReqDto: this.fb.array(this.documents),
+    });
+    this.activityOnInit();
   }
 
   get documentsReqDto() {
-    return this.scheduleForm.get('documentsReqDto') as FormArray
+    return this.scheduleForm.get('documentsReqDto') as FormArray;
   }
-  removeActivity(i : number) {
-    this.documentsReqDto.removeAt(i)
+  removeActivity(i: number) {
+    this.documentsReqDto.removeAt(i);
   }
 
   activityOnInit() {
-    this.documentsReqDto.push(this.fb.group({
-      activity : [{value: this.activityInit.at(0)?.activity, disabled: true, }, [Validators.required]],
-      documentDeadline: [this.activityInit.at(0)?.documentDeadline, [Validators.required]]
-    }))
-    this.documentsReqDto.push(this.fb.group({
-      activity : [{value: this.activityInit.at(1)?.activity, disabled: true, }, [Validators.required]],
-      documentDeadline: [this.activityInit.at(1)?.documentDeadline, [Validators.required]]
-    }))
-    this.documentsReqDto.push(this.fb.group({
-      activity : [{value: this.activityInit.at(2)?.activity, disabled: true, }, [Validators.required]],
-      documentDeadline: [this.activityInit.at(2)?.documentDeadline, [Validators.required]]
-    }))
+    this.documentsReqDto.push(
+      this.fb.group({
+        activity: [
+          { value: this.activityInit.at(0)?.activity, disabled: true },
+          [Validators.required],
+        ],
+        documentDeadline: [
+          this.activityInit.at(0)?.documentDeadline,
+          [Validators.required],
+        ],
+      })
+    );
+    this.documentsReqDto.push(
+      this.fb.group({
+        activity: [
+          { value: this.activityInit.at(1)?.activity, disabled: true },
+          [Validators.required],
+        ],
+        documentDeadline: [
+          this.activityInit.at(1)?.documentDeadline,
+          [Validators.required],
+        ],
+      })
+    );
+    this.documentsReqDto.push(
+      this.fb.group({
+        activity: [
+          { value: this.activityInit.at(2)?.activity, disabled: true },
+          [Validators.required],
+        ],
+        documentDeadline: [
+          this.activityInit.at(2)?.documentDeadline,
+          [Validators.required],
+        ],
+      })
+    );
   }
 
   addActivity() {
-    this.documentsReqDto.push(this.fb.group({
-      activity : ['', [Validators.required]],
-      documentDeadline: ['', [Validators.required]]
-    }))
+    this.documentsReqDto.push(
+      this.fb.group({
+        activity: ['', [Validators.required]],
+        documentDeadline: ['', [Validators.required]],
+      })
+    );
   }
 
-  convertToDate(dateString : string | null) {
+  convertToDate(dateString: string | null) {
     if (!dateString) {
-      return null
+      return null;
     }
-    let date = new Date(dateString)
-    return date
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    let date = new Date(year, month, day);
+    return date;
   }
 
-  setMaxDate(dateString : string | null) {
-    let maxDate = this.convertToDate(dateString)
-    maxDate?.setDate(maxDate.getDate() - 2)
-    return maxDate
+  setMinDate() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1;
+    const dateString = `01/${currentMonth}/2024`;
+
+    let minDate = this.convertToDate(dateString);
+    minDate?.setDate(minDate.getDate());
+    return minDate;
   }
 
-  formatDate(dateString : string | null) {
-    let payrollDate = this.convertToDate(dateString)
-    const day = String(payrollDate!.getUTCDate()).padStart(2, '0')
-    const month = String(payrollDate!.getUTCMonth() + 1).padStart(2, '0') // Months are zero-based
-    const year = payrollDate!.getUTCFullYear()
+  setMaxDate(dateString: string | null) {
+    let maxDate = this.convertToDate(dateString);
+    maxDate?.setDate(maxDate.getDate() - 2);
+    return maxDate;
+  }
 
-    return `${day}/${month}/${year}`
-}
+  onDateSelect(event: any, i: number) {
+    const selectedDate = new Date(event);
 
-onDateSelect(event:any, i: number) {
-  const selectedDate = new Date(event);
-  const dateString = selectedDate.toISOString()
+    const newDate = new Date(
+      Date.UTC(
+        selectedDate.getFullYear(),
+        selectedDate.getMonth(),
+        selectedDate.getDate(),
+        selectedDate.getHours(),
+        selectedDate.getMinutes(),
+        selectedDate.getSeconds()
+      )
+    );
+    const dateString = newDate.toISOString();
 
-  const documentDeadline = dateString.substring(0, dateString.length - 5);
-  this.documentsReqDto.at(i)?.patchValue({documentDeadline: documentDeadline})
-}
+    const documentDeadline = dateString.substring(0, dateString.length - 5);
+
+    this.documentsReqDto
+      .at(i)
+      ?.patchValue({ documentDeadline: documentDeadline });
+  }
 
   onBack() {
-    this.location.back()
+    this.location.back();
   }
 
   onSubmit() {
     if (this.scheduleForm.valid) {
-      const documentReqDto : DocumentReqDto = this.scheduleForm.getRawValue()
-      firstValueFrom(this.documentService.addPayrollSchedule(documentReqDto)).then(() => {
-        this.location.back()
-      })
+      const documentReqDto: DocumentReqDto = this.scheduleForm.getRawValue();
+      firstValueFrom(
+        this.documentService.addPayrollSchedule(documentReqDto)
+      ).then(() => {
+        this.location.back();
+      });
     }
   }
 }
--- src/app/pages/schedules/schedules.component.ts
+++ src/app/pages/schedules/schedules.component.ts
@@ -1,23 +1,28 @@
-import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
-import { FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
-import { ButtonModule } from "primeng/button";
-import { CalendarModule } from "primeng/calendar";
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { BadgeModule } from 'primeng/badge';
-import { AuthService } from "../../services/auth/auth.service";
-import { RouterModule } from "@angular/router";
-import { PayrollService } from "../../services/payroll.service";
+import { AuthService } from '../../services/auth/auth.service';
+import { RouterModule } from '@angular/router';
+import { PayrollService } from '../../services/payroll.service';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
-import { ToastModule } from "primeng/toast";
-import { MessageService } from "primeng/api";
-import { PayrollResDto } from "../../dto/payroll/payroll.res.dto";
-import { RoleType } from "../../constants/roles.constant";
-import { ScheduleStatusType } from "../../constants/schedule-request-types.constant";
-import { firstValueFrom } from "rxjs";
-import { ScheduleResDto } from "../../dto/schedule/schedule.res.dto";
-import { RescheduleReqDto } from "../../dto/schedule/reschedule.req.dto";
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { PayrollResDto } from '../../dto/payroll/payroll.res.dto';
+import { RoleType } from '../../constants/roles.constant';
+import { ScheduleStatusType } from '../../constants/schedule-request-types.constant';
+import { firstValueFrom } from 'rxjs';
+import { ScheduleResDto } from '../../dto/schedule/schedule.res.dto';
+import { RescheduleReqDto } from '../../dto/schedule/reschedule.req.dto';
 
 @Component({
   selector: 'app-schedules',
@@ -35,111 +40,131 @@ import { RescheduleReqDto } from "../../dto/schedule/reschedule.req.dto";
     RouterModule,
     DialogModule,
     InputTextModule,
-    ToastModule
+    ToastModule,
   ],
   providers: [MessageService],
 })
 export class Schedules implements OnInit {
-  roleCode : boolean = true
-  status: string = 'Pending'
-  dataLogin = this.authService.getLoginData()
+  roleCode: boolean = true;
+  status: string = 'Pending';
+  dataLogin = this.authService.getLoginData();
   visible: boolean = false;
-  schedules : PayrollResDto[] = []
-  clientSchedules : ScheduleResDto[] = []
-  maxDate: Date | null = null
+  schedules: PayrollResDto[] = [];
+  clientSchedules: ScheduleResDto[] = [];
+  maxDate: Date | null = null;
 
   rescheduleForm = this.fb.group({
-      scheduleId: ['', Validators.required],
-      newDeadline: ['', [Validators.required]]
-  })
+    scheduleId: ['', Validators.required],
+    newDeadline: ['', [Validators.required]],
+  });
 
   constructor(
-    private authService : AuthService,
-    private payrollService : PayrollService,
-    private fb : NonNullableFormBuilder,
+    private authService: AuthService,
+    private payrollService: PayrollService,
+    private fb: NonNullableFormBuilder
   ) {}
 
   ngOnInit(): void {
-    this.init()
+    this.init();
   }
 
   init() {
     if (this.rolePS) {
-      firstValueFrom(this.payrollService.getAllClients()).then(res => {
-        this.schedules = res
-      })
+      firstValueFrom(this.payrollService.getAllClients()).then((res) => {
+        this.schedules = res;
+      });
     }
     if (this.roleClient) {
-      firstValueFrom(this.payrollService.getLoginClientSchedule()).then(res => {
-        this.clientSchedules = res
-      })
+      firstValueFrom(this.payrollService.getLoginClientSchedule()).then(
+        (res) => {
+          this.clientSchedules = res;
+        }
+      );
     }
   }
 
   get rolePS() {
-    return this.dataLogin?.roleCode == RoleType.PS
+    return this.dataLogin?.roleCode == RoleType.PS;
   }
   get roleClient() {
-    return this.dataLogin?.roleCode == RoleType.CLIENT
+    return this.dataLogin?.roleCode == RoleType.CLIENT;
   }
 
-  showDialog(schedule : ScheduleResDto) {
-    this.rescheduleForm.reset()
+  showDialog(schedule: ScheduleResDto) {
+    this.rescheduleForm.reset();
     this.visible = true;
-    this.maxDate = this.setMaxDate(schedule.payrollDate)
+    this.maxDate = this.setMaxDate(schedule.payrollDate);
     this.rescheduleForm.patchValue({
-      scheduleId : schedule.scheduleId,
-    })
+      scheduleId: schedule.scheduleId,
+    });
   }
 
-  convertToDate(dateString : string | null) {
+  convertToDate(dateString: string | null) {
     if (!dateString) {
-      return null
+      return null;
     }
-    let date = new Date(dateString)
-    return date
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    let date = new Date(year, month, day);
+    return date;
   }
 
-  setMaxDate(dateString : string | null) {
-    let maxDate = this.convertToDate(dateString)
-    maxDate?.setDate(maxDate.getDate() - 2)
-    return maxDate
+  setMaxDate(dateString: string | null) {
+    let maxDate = this.convertToDate(dateString);
+    maxDate?.setDate(maxDate.getDate() - 2);
+    return maxDate;
   }
 
-  formatDate(dateString : string | null) {
-    let payrollDate = this.convertToDate(dateString)
-    const day = String(payrollDate!.getUTCDate()).padStart(2, '0')
-    const month = String(payrollDate!.getUTCMonth() + 1).padStart(2, '0') // Months are zero-based
-    const year = payrollDate!.getUTCFullYear()
+  formatDate(dateString: string | null) {
+    let payrollDate = this.convertToDate(dateString);
+    const day = String(payrollDate!.getUTCDate()).padStart(2, '0');
+    const month = String(payrollDate!.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const year = payrollDate!.getUTCFullYear();
 
-    return `${day}/${month}/${year}`
-}
-
-  isCompleted(i : number) {
-    if (this.schedules.at(i)?.scheduleStatusCode == ScheduleStatusType.COMPLETED) {
-      return true
-    } else if (this.clientSchedules.at(i)?.scheduleStatusCode == ScheduleStatusType.COMPLETED) {
-      return true
-    }
-    return false
+    return `${day}/${month}/${year}`;
   }
 
-  isNoSchedule(i : number) {
-    if (this.schedules.at(i)?.scheduleStatusCode == ScheduleStatusType.NO_SCHEDULE) {
-      return true
-    } else if (this.clientSchedules.at(i)?.scheduleStatusCode == ScheduleStatusType.NO_SCHEDULE) {
-      return true
+  isCompleted(i: number) {
+    if (
+      this.schedules.at(i)?.scheduleStatusCode == ScheduleStatusType.COMPLETED
+    ) {
+      return true;
+    } else if (
+      this.clientSchedules.at(i)?.scheduleStatusCode ==
+      ScheduleStatusType.COMPLETED
+    ) {
+      return true;
     }
-    return false
+    return false;
+  }
+
+  isNoSchedule(i: number) {
+    if (
+      this.schedules.at(i)?.scheduleStatusCode == ScheduleStatusType.NO_SCHEDULE
+    ) {
+      return true;
+    } else if (
+      this.clientSchedules.at(i)?.scheduleStatusCode ==
+      ScheduleStatusType.NO_SCHEDULE
+    ) {
+      return true;
+    }
+    return false;
   }
 
   onSubmit() {
     this.rescheduleForm.patchValue({
-      newDeadline: this.formatDate(this.rescheduleForm.getRawValue().newDeadline)
-    })
-    const request : RescheduleReqDto = this.rescheduleForm.getRawValue()
-    firstValueFrom(this.payrollService.createRescheduleRequest(request)).then(res =>{
-      this.visible = false
-    })
+      newDeadline: this.formatDate(
+        this.rescheduleForm.getRawValue().newDeadline
+      ),
+    });
+    const request: RescheduleReqDto = this.rescheduleForm.getRawValue();
+    firstValueFrom(this.payrollService.createRescheduleRequest(request)).then(
+      (res) => {
+        this.visible = false;
+      }
+    );
   }
 }
